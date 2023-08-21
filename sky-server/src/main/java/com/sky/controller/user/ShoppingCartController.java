59494022dd7d8ae00cf4b68b56ea97e7b823f700
ChenXW <chenxw1024@163.com>
2023-08-21 11:49:53 +0800

    完成购物车模块代码开发

---
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -0,0 +1,60 @@
+package com.sky.controller.user;
+
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+import com.sky.result.Result;
+import com.sky.service.ShoppingCartService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user/shoppingCart")
+@Slf4j
+@Api(tags = "C端-购物车接口")
+public class ShoppingCartController {
+
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    /**
+     * @description: 添加购物车
+     * @author: ChenXW
+     * @date: 2023/8/21 10:54
+     */
+    @PostMapping("/add")
+    @ApiOperation("添加购物车")
+    public Result<String> add(@RequestBody ShoppingCartDTO shoppingCartDTO) {
+        log.info("添加购物车：{}", shoppingCartDTO);
+        shoppingCartService.addShoppingCart(shoppingCartDTO);//后绪步骤实现
+        return Result.success();
+    }
+
+    /**
+     * @description: 查看购物车
+     * @author: ChenXW
+     * @date: 2023/8/21 11:39
+     */
+    @GetMapping("/list")
+    @ApiOperation("查看购物车")
+    public Result<List<ShoppingCart>> list(){
+        return Result.success(shoppingCartService.showShoppingCart());
+    }
+
+    /**
+     * @description: 清空购物车商品
+     * @author: ChenXW
+     * @date: 2023/8/21 11:47
+     */
+    @DeleteMapping("/clean")
+    @ApiOperation("清空购物车商品")
+    public Result<String> clean(){
+        shoppingCartService.cleanShoppingCart();
+        return Result.success();
+    }
+
+}
